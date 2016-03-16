1716fd1cff663b955d1b83cf494888474fedf014
Clace-C <Clace-C@users.noreply.github.com>
2016-03-16 03:14:27 -0500

    Added files via upload

--- ROSE/app/src/androidTest/java/comp3350/rose/ROSEUnitTest.java
+++ ROSE/app/src/androidTest/java/comp3350/rose/ROSEUnitTest.java
@@ -6,12 +6,22 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import org.junit.Test;
+import org.junit.*;
 
-import comp3350.rose.model.Recipe;
-import comp3350.rose.Stub.StubDB;
+import android.content.Context;
+import android.app.ListActivity;
+import android.util.Log;
 
 import java.util.ArrayList;
+
+import comp3350.rose.Controller.DBInterface;
+import comp3350.rose.model.Recipe;
+import comp3350.rose.Stub.StubDB;
+import comp3350.rose.Database.RecipeDatabase;
+import comp3350.rose.Business.MyApplication;
+import comp3350.rose.Controller.DBInterface;
+import comp3350.rose.model.Recipe;
+import comp3350.rose.Stub.StubDB;
 
 import dalvik.annotation.TestTargetClass;
 import static org.junit.Assert.*;
@@ -20,21 +30,34 @@ import static org.junit.Assert.*;
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
 public class ROSEUnitTest {
+    static Context con;
+    static StubDB stub;
+    static ArrayList<Recipe> temp_recipes;
+    static ArrayList<String> temp_recipeDisplay;
+    static int temp_recipePosition;
+    static Recipe temp_Recipe;
+    static String temp_string;
+    static int temp_int;
+
     @BeforeClass
     public static void setUpClass() throws Exception {
-        ArrayList<Recipe> temp_recipes = StubDB.getList();
-        ArrayList<String> temp_recipeDisplay = new ArrayList<>();
+        con = MyApplication.getAppContext();
+        stub = StubDB.getInstance(con);
 
-        int temp_recipePosition = 0;
-        Recipe temp_Recipe;
 
-        String temp_string;
-        int temp_int;
+        temp_recipes = stub.getList();
+        temp_recipeDisplay = new ArrayList<>();
+
+        temp_recipePosition = 0;
+
     }
 
     @Test
     protected void initTest(Bundle savedInstanceState) {
-        init(savedInstanceState);
+        init start = new init();
+
+        ArrayList<Recipe> recipes = stub.getList();
+        ArrayList<String> recipeDisplay = new ArrayList<>();
 
         assertEquals("Recipes were not loaded from the stub database correctly.", temp_recipes, recipes);
         assertEquals("Resipe display list not loaded correctly.", temp_recipeDisplay, recipeDisplay);
@@ -43,7 +66,10 @@ public class ROSEUnitTest {
 
     @Test
     protected void DetailsTest(Bundle savedInstanceState) {
-        Details(savedInstanceState);
+        Details det = new Details();
+
+        Recipe clickedRecipe = ((MyApplication)det.getApplication()).getSortedRecipes().get(det.recipePosition);
+        ArrayList<String> recipeDetails = new ArrayList<>();
 
         assertEquals("Clicked Recipe is not a valid Recipe.", temp_Recipe, clickedRecipe);
         assertEquals("Recipe Details not in valid String ArrayList format.", temp_recipeDisplay, recipeDetails);
@@ -52,6 +78,19 @@ public class ROSEUnitTest {
 
     @Test
     protected void RecipeTest(Bundle savedInstanceState) {
+        Details det = new Details();
+        Recipe example = ((MyApplication)det.getApplication()).getSortedRecipes().get(det.recipePosition);
+
+        String name = example.getName();
+        String mealType = example.getMealType();
+        String mainIngredient = example.getMainIngredient();
+        int rating = example.getRating();
+        String description = example.getDescription();
+        ArrayList<String> ingredients = example.getIngredients();
+        ArrayList<String> directions = example.getDirections();
+        int cooktime = example.getCooktime();
+        String notes = example.getNotes();
+
         assertEquals("Name is not a valid String.", name, temp_string);
         assertEquals("Meal Type is not a valid String.", mealType, temp_string);
         assertEquals("Main Ingredient is not a valid String.", mainIngredient, temp_string);
@@ -66,9 +105,30 @@ public class ROSEUnitTest {
 
     }
 
+    @Test
+    protected void ShoppingListTest(Bundle savedInstanceState) {
+        ShoppingList list = new ShoppingList();
+        ArrayList<String> shoppingDisplay = new ArrayList<>();
+
+        assertEquals("Shopping List display list not loaded correctly.", temp_recipeDisplay, shoppingDisplay);
+
+    }
+
+    @Test
+    protected void EditTest(Bundle savedInstanceState) {
+        Edit changer = new Edit();
+
+        Recipe recipeToModify = changer.recipeToModify;
+
+        assertEquals("Chosen Recipe is not a valid Recipe.", temp_Recipe, recipeToModify);
+
+
+    }
+
     @AfterClass
     public static void tearDownClass() throws Exception {
         // No Cleanup Necessary
     }
 
 }
+
