41b5b52e9be74f688d0f16f5b34b4bb52f16f455
Kev <krapht@gmail.com>
2016-03-27 17:47:08 -0500

    Includes add recipes branch and some gui additions
    working version with add recipes and title page

--- ROSE/app/src/main/java/comp3350/rose/Presentation/init.java
+++ ROSE/app/src/main/java/comp3350/rose/Presentation/init.java
@@ -1,86 +1,33 @@
 package comp3350.rose.Presentation;
 
-import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.widget.Toast;
 
-import java.util.ArrayList;
-
-import comp3350.rose.Controller.DBInterface;
 import comp3350.rose.R;
-import comp3350.rose.model.Recipe;
-import comp3350.rose.Business.MyApplication;
 
-public class init extends ListActivity {
+public class init extends AppCompatActivity {
 
-    ListView lv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_init);
-        //Get Database to have access to arraylist of stored recipes
-        DBInterface repository = ((MyApplication)this.getApplication()).getRepository(this);
-
-        ArrayList<Recipe> recipes = repository.getList();
-        ((MyApplication)this.getApplication()).setSortedRecipes(recipes); //initialize sortedRecipes to full recipe list
-        ArrayList<String> recipeDisplay = new ArrayList<>();
-        for(int i=0; i<recipes.size(); i++) {
-            recipeDisplay.add(recipes.get(i).getName());
-        }
-
-        ArrayAdapter<String> myArrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, recipeDisplay);
-        lv = this.getListView();
-        lv.setAdapter(myArrayAdapter);
     }
 
-    @Override
-    protected void onResume() { //TODO Flesh this out more
-        //Same as the DetaiLView mode
-        super.onResume();
-        refreshList();
+    public void menuButton(View view){
+        Toast.makeText(getApplicationContext(), "Currently in Development.", Toast.LENGTH_LONG).show();
     }
 
-    @Override
-    protected void onListItemClick(ListView l, View v, int pos, long id){
-        super.onListItemClick(l, v, pos, id);
-
-        Intent myIntent = new Intent(this, Details.class);
-        myIntent.putExtra("recipePosition", pos);
+    public void viewRecipesButton(View view){
+        Intent myIntent = new Intent(this, ViewRecipes.class);
         startActivity(myIntent);
     }
 
-    public void shoppingListButton(View view){
-        startActivity(new Intent(this, SLDetails.class));
-    }
-
-    public void addRecipeButton(View view){
-        Intent myIntent = new Intent(this, Edit.class);
-        myIntent.putExtra("editType", 1); // 1 corresponds to add recipe
+    public void viewShoppingListButton(View view){
+        Intent myIntent = new Intent(this, SLDetails.class);
         startActivity(myIntent);
     }
-
-    public void sortButton(View view){
-        Intent myIntent = new Intent(this, SortRecipes.class);
-        startActivity(myIntent);
-    }
-
-    private void refreshList(){//TODO Flesh this out more
-        //Same as the DetailView mode
-        //DBInterface repository = ((MyApplication)this.getApplication()).getRepository(this);
-
-        ArrayList<Recipe> recipes = ((MyApplication)this.getApplication()).getSortedRecipes(); //refresh with sortedRecipes
-        ArrayList<String> recipeDisplay = new ArrayList<>();
-        for(int i=0; i<recipes.size(); i++) {
-            recipeDisplay.add(recipes.get(i).getName());
-        }
-
-        ArrayAdapter<String> myArrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, recipeDisplay);
-        lv = this.getListView();
-        lv.setAdapter(myArrayAdapter);
-    }
-
 
 }
